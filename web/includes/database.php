7557ba0b01874cb5eff418ea332e5de77b11e35c
demonickrace <krace710267@gmail.com>
2016-04-13 13:56:48 +0800

    first commit

---
+++ web/includes/database.php
@@ -0,0 +1,40 @@
+<?
+  class DBase 
+  {
+      function defaultDBConnect() 
+	  {
+	      mysql_connect("localhost", "root", "1234qwer") or die ( "無法連結" );
+          mysql_select_db("asgs") or die ( "無法選擇資料庫" );
+      }
+      
+  	  function DBConnect( $dbhost, $dbadm, $dbpwd, $dbname )
+	  {
+	      mysql_connect( $dbhost ,$dbadm, $dbpwd ) or die ( "無法連結" );
+          mysql_select_db( $dbname ) or die ( "無法選擇資料庫" );
+	  }
+	  
+	  function Query($querystr)
+	  {
+          $identify = mysql_query( $querystr );
+	   	  return $identify;
+      }
+        
+	  function Fetch_array( $identify )
+	  {
+          $row = mysql_fetch_array( $identify );
+          return $row;
+	  }
+	  
+  }
+  
+  $dbhost = "localhost";
+  $dbname = 'grading';
+  $dbadm  = 'root';
+  $dbpwd  = '1234qwer';
+
+  $db = new DBase();
+  $db->DBConnect( $dbhost ,$dbadm, $dbpwd, $dbname );
+  $db->Query( "SET NAMES 'UTF-8'" );
+
+    
+?>
